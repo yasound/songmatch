1db6386e3557af8fd2d2a6671f53fd17f809c4b4
Sebastien Metrot <meeloo@meeloo.net>
2011-11-11 14:34:57 +0100

    added csv dump of 700K songs database

--- src/songmatch.py
+++ src/songmatch.py
@@ -1,2 +1,16 @@
-import os
+import csv, sys
+
+def read_csv(filename):
+  with open(filename, 'rb') as f:
+    reader = csv.reader(f)
+    try:
+      for row in reader
+        print row
+    except csv.Error, e:
+      sys.exit('file %s, line %d: %s' % (filename, reader.line_num, e))
+  
+print 'Dump csv\n'
+read_csv('dump.csv')
+
+print 'DONE\n'
 
